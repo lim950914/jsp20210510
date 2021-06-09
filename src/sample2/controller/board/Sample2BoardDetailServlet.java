3bf2aedcef7c59b10fe6d12c345a4b92dfeb7992
lim950914 <dlackswn2222@naver.com>
2021-06-09 15:27:47 +0900

    20210609

--- src/sample2/controller/board/Sample2BoardDetailServlet.java
+++ src/sample2/controller/board/Sample2BoardDetailServlet.java
@@ -1,6 +1,7 @@
 package sample2.controller.board;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import sample2.bean.BoardDto;
+import sample2.been.Comment;
 import sample2.dao.BoardDao;
+import sample2.service.comment.CommentService;
 
 /**
  * Servlet implementation class Sample2BoardDetailServlet
@@ -17,7 +20,9 @@ import sample2.dao.BoardDao;
 @WebServlet("/sample2/board/detail")
 public class Sample2BoardDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+      
+	private CommentService commentService;
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,7 +30,14 @@ public class Sample2BoardDetailServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
+    @Override
+    public void init() throws ServletException {
+    	// TODO Auto-generated method stub
+    	super.init();
+    	this.commentService = new CommentService();
+    }
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -40,7 +52,9 @@ public class Sample2BoardDetailServlet extends HttpServlet {
 //			Board board = dao.get(Integer.parseInt(id));
 			BoardDto board = dao.get2(Integer.parseInt(id));
 			
+			List<Comment> commentList = commentService.list(Integer.parseInt(id));
 			request.setAttribute("board", board);
+			request.setAttribute("comments", commentList);
 			
 			String path = "/WEB-INF/sample2/board/detail.jsp";
 			request.getRequestDispatcher(path).forward(request, response);
